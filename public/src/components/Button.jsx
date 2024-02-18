5b399c118dfb94a596621994f721d98445afe37c
kunal <kunalgarg054@gmail.com>
2024-02-18 19:07:16 +0530

    commit

--- public/src/components/Button.jsx
+++ public/src/components/Button.jsx
@@ -1,8 +1,8 @@
 
-export function Button({label}){
+export function Button({label, onClick}){
     return (
         <div>
-            <button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 w-full">
+            <button onClick={onClick} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 w-full" >
                 {label}
             </button>
         </div>
